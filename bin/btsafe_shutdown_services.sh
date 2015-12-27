35699dddd4c9379ac2645ef1d1a98b750bf0cebc
root <you@example.com>
2015-12-27 21:31:30 +0100

    add missing files

---
+++ bin/btsafe_shutdown_services.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# CONFIGURATION:
+
+source /usr/local/bin/btmain.inc.sh
+LOG_FILE="/var/log/btsafeshutdown.log"
+
+
+# IMPLEMENTATION:
+
+function try_clean_stop {
+         /etc/init.d/btmain stop
+         /etc/init.d/rtorrentdaemon stop
+         /etc/init.d/aria2 stop
+         /etc/init.d/mldonkey-server stop
+         /etc/init.d/minidlna stop
+         /etc/init.d/samba stop
+         sleep 4
+}
+
+function check_if_something_still_running {
+         still_running=""
+         pgrep btmain; if [[ "$?" = "0" ]]; then still_running=btmain; return; fi
+         pgrep rtorrent; if [[ "$?" = "0" ]]; then still_running=rtorrent; return; fi
+         pgrep aria2c; if [[ "$?" = "0" ]]; then still_running=aria2c; return; fi
+         pgrep mlnet; if [[ "$?" = "0" ]]; then still_running=mlnet; return; fi
+         pgrep minidlna; if [[ "$?" = "0" ]]; then still_running=minidlna; return; fi
+}
+
+function do_force_stop {
+         killall -9 btmain.sh
+         killall -9 rtorrent
+         killall -9 aria2c
+         killall -9 mlnet
+         killall -9 minidlnad
+         killall -9 smbd
+         killall -9 nmbd
+         sleep 2
+}
+
+
+
+# get arguments
+FINAL_ACTION="$1"
+
+
+# init Bash Shell Function Library (BSFL)
+source /usr/local/bin/bsfl
+START=`now`
+
+sleep 2
+
+msg '*** Shutting down all core services in the correct order:'
+try_clean_stop
+check_if_something_still_running
+if [ ! -z "$still_running" ]; then
+     msg "*** Detected some service still running: $still_running.... retrying a clean shutdown"
+     try_clean_stop  
+fi
+
+check_if_something_still_running
+if [ ! -z "$still_running" ]; then
+     msg "*** Stuff is still running: $still_running... killing them"
+     do_force_stop
+fi
+
+check_if_something_still_running
+if [ ! -z "$still_running" ]; then
+     msg "*** Stuff is still running: $still_running... all attempts failed. aborting."
+     exit 1
+fi
+
+msg '*** All services correctly shut down... unmounting external discs'
+umount /media/extdisc*
+sleep 5
+       
+msg '*** Shut down of services completed'
+sync
+sleep 1
+
+if [[ $FINAL_ACTION == "reboot" ]]; then
+    msg '*** Performing final reboot'
+    shutdown -r now
+elif [[ $FINAL_ACTION == "halt" ]]; then
+    msg '*** Performing final halt'
+    shutdown -h now
+fi
+
