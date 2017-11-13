035ede3740c74ac6109502441534f41d282a8fca
root <root@localhost>
2017-11-13 12:15:23 +0100

    change code to use new PHP library from microirrigation-control project

--- web/irrigation-start.php
+++ web/irrigation-start.php
@@ -5,19 +5,28 @@
 
   include 'inc/page-utils.php';
   include 'inc/page-header.php';
+  include 'inc/lime2node_over_spi.php';
 
   if ( $is_authorized )
   {
     echo "<p>Micro-irrigation starting:</p>";
-	echo "<p style='text-align:center; font-size: 200%; font-family: monospace;'><strong>";
-	//$command = escapeshellcmd('/opt/microirrigation-control/bin/lime2_valve_ctrl.py open 2>&1');
-	
-	// NOTE: since the Python script will employ GPIO module, it will need ROOT permissions:
-	$command = 'sudo /opt/microirrigation-control/bin/lime2_valve_ctrl.py open 2>&1';
-	$output = shell_exec($command);
-	echo $output;
+  	echo "<p style='text-align:center; font-size: 200%; font-family: monospace;'><strong><tt>";
+  	//$command = escapeshellcmd('/opt/microirrigation-control/bin/lime2_valve_ctrl.py open 2>&1');
+  	
+  	// NOTE: since the Python script will employ GPIO module, it will need ROOT permissions:
+  	/*$command = 'sudo /opt/microirrigation-control/bin/lime2_valve_ctrl.py open 2>&1';
+  	$output = shell_exec($command);
+  	echo $output;*/
+   
+   
+    init_lime2node_over_spi();
+      
+    $tid = get_last_transaction_id_and_advance();
+    $ack_contents = array();
+    if (send_spi_cmd($turnon_cmd, $tid, $ack_contents))
+      wait_for_ack($tid);
 	   
-    echo "</strong></p><br/><br/>";
+    echo "</tt></strong></p><br/><br/>";
   }
 
   include 'inc/link-to-home.php';
